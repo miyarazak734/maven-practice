50520414d58f4f6ae584488d953431b740ad4218
RAZAK MOHAMMED <Razak>
2022-03-23 11:20:54 +0530

    New changes

---
+++ src/main/java/novsale/App.java
@@ -0,0 +1,13 @@
+package novsale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
